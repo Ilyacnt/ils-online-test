5cd3c582afd81884952bac57fac0c75cb600299a
Ilyacnt <ilyagt2012@gmail.com>
2023-07-25 12:27:03 +0300

    CSS Modules TS fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import './styles.css'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import Layout from './components/layout/Layout'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(<Layout />)
