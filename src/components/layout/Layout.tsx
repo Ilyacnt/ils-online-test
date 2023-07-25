196cb2f76ab9744c25b1c771e59b9165953d70da
Ilyacnt <ilyagt2012@gmail.com>
2023-07-25 12:35:52 +0300

    Layout base added

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,12 @@
 import styles from './Layout.module.css'
 
 const Layout = () => {
-    return <div className={styles.Layout}>Layout</div>
+    return (
+        <div className={styles.Layout}>
+            <div className={styles.Sidebar}>Sidebar</div>
+            <div className={styles.Content}>Content</div>
+        </div>
+    )
 }
 
 export default Layout
